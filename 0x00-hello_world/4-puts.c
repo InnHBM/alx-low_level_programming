160a1dd68244c161d8ea7479b71c1548dbe512c3
InnHBM <innocentrichard401@gmail.com>
2022-09-09 01:32:27 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+1 #include <stdio.h>
+2
+3 /**
+4 * main - Entry point
+5 *
+6 * Return: Always 0 (Success)
+7 */
+8 int main(void)
+9 {
+	puts("\"Programming is like bulding a multilingual puzzle");
+	return (0);
+12 }
+
