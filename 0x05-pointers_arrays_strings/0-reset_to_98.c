fc8ab05fcaceccbc4d2dafaebb1c46451668ace8
InnHBM <innocentrichard401@gmail.com>
2022-09-19 17:54:07 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
  * reset_t0_98 - resets argument to 98
- *
  * @n: pointer
  *
+ * Description: return the required result
+ *
  * Return: void
+ *
  */
 
 void reset_to_98(int *n)
