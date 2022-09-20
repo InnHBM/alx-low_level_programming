09cf34aef8a3e358da06bd96ad77170ffa97cc8c
InnHBM <innocentrichard401@gmail.com>
2022-09-19 17:34:43 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - print out string to the stdout
+ * @str: argument to the function
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
