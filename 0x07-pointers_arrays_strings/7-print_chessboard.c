d56efd08bf09d90876c44fb2a5cdc0d1f4329894
InnHBM <innocentrichard401@gmail.com>
2022-09-26 04:42:43 -0700

    print the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,12 +9,11 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i < *; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i]);
-			_putchar(a[j]);
+			_putchar(a[i][j]);
 		}
 		_putchar('\n');
 	}
